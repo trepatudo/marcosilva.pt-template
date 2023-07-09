7bcf7244729b8e94ffab2d198f1686c924a28d9f
Marco Silva <dev@marcosilva.pt>
2023-07-09 16:47:24 +0100

    feat: add theme switcher and improve content

---
+++ jekyll/assets/main.js
@@ -0,0 +1,34 @@
+/**
+ * Some crappy JS-only hax for theme selection
+ * @author Marco Silva
+ */
+
+const linkSelector = document.getElementById("themeSelectorLink");
+const themes = ["dark", "lite", "hack"];
+
+function switchTheme() {
+    let currentTheme = linkSelector.innerText;
+    let nextTheme = themes.indexOf(currentTheme) + 1;
+    nextTheme = nextTheme > 2 ? 0 : nextTheme;
+
+    // Change theme
+    enableTheme(themes[nextTheme]);
+}
+
+function enableTheme(theme) {
+    document.getElementsByTagName("body")[0].className = "theme-" + theme;
+    linkSelector.innerText = theme;
+    if (localStorage) {
+        localStorage.setItem("theme", theme);
+    }
+}
+
+
+linkSelector.addEventListener("click", function (event) {
+    switchTheme();
+});
+
+// Enable theme
+if (localStorage && localStorage.getItem("theme")) {
+    enableTheme(localStorage.getItem("theme"));
+}
